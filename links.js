e68b7f305be379f35ed6c78d7fced1d375d3359e
sumonkaysar <msk.sumon.kaysar@gmail.com>
2024-06-14 18:08:54 +0600

    changed server link

--- links.js
+++ links.js
@@ -1,6 +1,6 @@
 // server url
-export const server = "http://localhost:5000";
-// export const server = "https://talika-server-zoxh.onrender.com";
+// export const server = "http://localhost:5000";
+export const server = "https://talika-server-zoxh.onrender.com";
 
 // imgbb url
 export const imgbbUrl = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_imgbbKey}`;
